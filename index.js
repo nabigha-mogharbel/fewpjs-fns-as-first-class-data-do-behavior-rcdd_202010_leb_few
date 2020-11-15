3f5c0637929241bf276914d7f4a7435e2611925c
Nabigha Mogharbel <nabiga.ba.mo1998@gmail.com>
2020-11-15 11:39:48 +0000

    Done.

--- index.js
+++ index.js
@@ -8,6 +8,22 @@ function handleClick(e) {
 }
 
 /* End Given Code, don't edit above here...*/
+function greet(u){
+  let text=u.split(':');
+  var sen="";
+  let time=parseInt(text, 10);
+  if (time<12){
+    sen="Good Morning";}
+  else if (time>12 && time<17){sen="Good Afternoon";}
+  else {
+    sen="Good Evening";
+  }
+  return(sen);
+}
+//console.log(greet("18:30"))
 
 /* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
+function displayMessage(sen){
+  document.getElementById('greeting').innerText=sen
+}
